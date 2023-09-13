4cff4a7b48b6939c8ed8080cbbd2e9430bf637df
Trym <hamretrym@gmail.com>
2023-09-13 12:25:08 +0200

    TEST

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
 import sqlite3
+<<<<<<< Updated upstream
 con = sqlite3.connect("Turguid.sql")
+=======
+con = sqlite3.connect("tutoria.sql")
+>>>>>>> Stashed changes
 cur = con.cursor()
 cur.execute("CREATE TABLE movie(title, year, score)")
 res = cur.execute("SELECT name FROM sqlite_master")
