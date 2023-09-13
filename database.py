c2cde74a3653a12af283476015dd2a019ea959cf
Trym <hamretrym@gmail.com>
2023-09-13 12:22:34 +0200

    TEST

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlite3
-con = sqlite3.connect("tutorial.sql")
+con = sqlite3.connect("Turguid.sql")
 cur = con.cursor()
 cur.execute("CREATE TABLE movie(title, year, score)")
 res = cur.execute("SELECT name FROM sqlite_master")
