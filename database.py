9bc4878db17c6aef2d3967443a82b5b0609ab4b2
Trym <hamretrym@gmail.com>
2023-09-13 11:49:16 +0200

    hello

---
+++ database.py
@@ -0,0 +1,10 @@
+import sqlite3
+con = sqlite3.connect("tutorial.sql")
+cur = con.cursor()
+cur.execute("CREATE TABLE movie(title, year, score)")
+res = cur.execute("SELECT name FROM sqlite_master")
+res.fetchone()
+con.commit()
+res = cur.execute("SELECT score FROM movie")
+res.fetchall()
+print(res) 
