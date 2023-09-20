9f46d6d02f381ba09d465938a4d24170cc94f139
Trym <hamretrym@gmail.com>
2023-09-20 14:17:39 +0200

    Tobias glad :)

--- src/userdata/Database/email.py
+++ src/userdata/Database/email.py
@@ -13,8 +13,8 @@ msg = EmailMessage()
 msg['Subject'] = 'Velkommen til safaritur'
 #Lag en ekte email kanskje?
 msg['From'] = "Admin-mail"
-#DETTE SKAL FUNGERE I TEORIEN MEN IDK
-msg['To'] = db.email_henter
+#DETTE SKAL FUNGERE I TEORIEN MEN MÅ HA VARIABLET FRA TKINTER NÅR HAN LOGGER INN
+msg['To'] = db.email_henter("brukernavn")
 msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
 
 # Open the files in binary mode.  You can also omit the subtype
