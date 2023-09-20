c32f44b57bfca7d38f5895d6a7ad21761e4d97b1
mapclean9r <tobias.the@hotmail.no>
2023-09-20 10:21:20 +0200

    update map

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
-def hello():
-    print("hello")
+from setuptools import setup
 
-
-hello()
+setup(
+    name="mittprosjekt",
+    version="1.0.0",
+    author="Ditt Navn",
+    author_email="din@email.com",
+    description="En beskrivelse av mitt prosjekt",
+    packages=["mittprosjekt"],
+    install_requires=[
+        "",
+        "",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
