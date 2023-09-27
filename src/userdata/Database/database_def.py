ac1934edfaab8aa3eebd5e6a542a6cc08c6a35d0
Trym <hamretrym@gmail.com>
2023-09-27 13:56:41 +0200

    oppdatert
    ny fungerende funskjoner

--- src/userdata/Database/database_def.py
+++ src/userdata/Database/database_def.py
@@ -7,8 +7,13 @@ test = cur.execute("SELECT ID FROM Person")
 #Bruk denne når man skal logge inn 
 #Finner ID til brukeren som er akkurat innlogget
 def finne_id_bruker(bruker):
-     id = cur.execute("SELECT ID FROM Bruker_konto WHERE BRUKERNAVN == (?)",bruker)
-     return id
+    cur.execute("SELECT ID FROM Bruker_konto WHERE BRUKERNAVN = ?", (bruker,))
+    result = cur.fetchone() 
+    if result:
+        return result[0]
+    else:
+        return -1  
+
 
 def finne_tur_id():
      pass
@@ -20,25 +25,30 @@ def lag_person(fornavn,etternavn,age,):
 
 
 #Funksjonen for å lage kontoer
-def lag_brukerkonto(brukernavn, passord,email,isadmin):
-    #Bruker = cur.execute("SELECT BRUKERNAVN FROM Bruker_konto WHERE (?)==",brukernavn)
-    #allerede = Bruker.fetchall
-    #if brukernavn in allerede:
-    #    return ("Dette brukernavnet er allede i bruk")
-    #else:
-        cur.execute("INSERT INTO Bruker_konto(BRUKERNAVN,PASSORD,EMAIL,IS_admin) VALUES (?,?,?,?)",(brukernavn, passord,email,isadmin))
+def lag_brukerkonto(brukernavn, passord, email, isadmin):
+    cur.execute("SELECT COUNT(*) FROM Bruker_konto WHERE BRUKERNAVN = ?", (brukernavn,))
+    user_count = cur.fetchone()[0]
+    
+    if user_count > 0:
+        return "Dette brukernavnet er allerede i bruk"
+    
+    
+    cur.execute("INSERT INTO Bruker_konto(BRUKERNAVN, PASSORD, EMAIL, IS_admin) VALUES (?, ?, ?, ?)", (brukernavn, passord, email, isadmin))
+    return "Brukerkonto opprettet suksessfullt" 
 
-def kobling_bruker_person(bruker_id,person_id):#Putt inn tkinter bruker input i parantesen
-    Navn_bruker = cur.execute("SELECT BRUKERNAVN FROM Bruker_konto WHERE BRUKERNAVN = (?)",())
-    ID_bruker = cur.execute("SELECT ID FROM Bruker_konto WHERE Brukernavn = (?)",(bruker_id))
-    ID_bruk = ID_bruker.fetchall()
-    ID_person = cur.execute("SELECT ID FROM Person WHERE Brukernavn = (?)",(person_id))
-    ID_pers = ID_person.fetchall()
-    cur.execute("INSERT INTO kobling_person_Bruker(ID_Bruker,ID_Person) VALUES (?,?)",(ID_bruk,ID_pers))
+def kobling_bruker_person(bruker_id, person_id):
+    ID_bruker = cur.execute("SELECT ID FROM Bruker_konto WHERE Brukernavn = ?", (bruker_id,))
+    ID_person = cur.execute("SELECT ID FROM Person WHERE Brukernavn = ?", (person_id,))
+    ID_bruk = ID_bruker.fetchone()
+    ID_pers = ID_person.fetchone()
+    if ID_bruk and ID_pers:
+        cur.execute("INSERT INTO kobling_person_Bruker(ID_Bruker, ID_Person) VALUES (?, ?)", (ID_bruk[0], ID_pers[0]))
+    else:
+        print("User or person not found.")
 
 
-def lag_tur_info(sted,navn,beskrivelse):
-    cur.execute("INSERT INTO Tur_info(STED,NAVN,BESKRIVELSE)",(sted,navn,beskrivelse))
+def lag_tur_info(sted, navn, beskrivelse):
+    cur.execute("INSERT INTO Tur_info(STED, NAVN, BESKRIVELSE) VALUES (?, ?, ?)", (sted, navn, beskrivelse))
 
 def email_henter(brukernavn):
      cur.execute("SELECT EMAIL FROM Bruker_konto WHERE ID == (?)", finne_id_bruker(brukernavn))
@@ -51,12 +61,12 @@ def email_henter(brukernavn):
 
 
 #lag_brukerkonto("Tam","dan","FAKE@Fakis",1)
-variabelnavn = "dummy"
+variabelnavn = "Tam"
 
-res = cur.execute("SELECT BRUKERNAVN FROM Bruker_konto WHERE ID==(?)",finne_id_bruker(variabelnavn))#Variabelnavn skal bli byttet til hva variabelen som holder på brukeren sin brukernavn 
-ID = res.fetchall()
-for x in ID:
-    print(x)
+#Variabelnavn skal bli byttet til hva variabelen som holder på brukeren sin brukernavn
+res = cur.execute("SELECT ? FROM Bruker_konto", (str(finne_id_bruker(variabelnavn)),))
+ID = res.fetchone()
+print(ID)
 
 
 con.commit()
