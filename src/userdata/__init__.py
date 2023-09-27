cfcd3e2494c4b442ae5d1fcfe0e73af351e80877
mapclean9r <tobias.the@hotmail.no>
2023-09-27 14:44:45 +0200

    test init

--- src/userdata/__init__.py
+++ src/userdata/__init__.py
@@ -1,9 +1,12 @@
 from Database.database_def import *
 
+lag_brukerkonto("Tam", "dan", "fake@xD.no", 0)
+
 x = ""
 print("Skriv brukernavn")
 input(str(x))
 finne_id_bruker(x)
+
 
 if __name__ == '__main__':
 
