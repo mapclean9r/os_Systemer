e44b7f74335f9dd652b19a350d92d06b8b15eda7
Trym <hamretrym@gmail.com>
2023-09-28 13:21:56 +0200

    main ready
    skal være klar til å merge til main

--- src/userdata/__init__.py
+++ src/userdata/__init__.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.insert(0, r'C:\Users\hamre\OneDrive\Dokumenter\GitHub\os_Systemer\src\userdata\Database\bruker_def.py')
-
 from userdata.Database.bruker_def import lag_brukerkonto
 
 lag_brukerkonto("DAWDWA", "NEI", "DWDA", 0)
