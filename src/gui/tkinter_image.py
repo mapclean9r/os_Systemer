7e304bc6f03155f704ec8f9c1298f8dc788cec3a
HomandoBruther <76850754+HomandoBruther@users.noreply.github.com>
2023-09-29 09:31:41 +0200

    tkinter legg til bilde test i gui

---
+++ src/gui/tkinter_image.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+
+# Create the main application window
+app = tk.Tk()
+app.title("Image Viewer")
+
+
+# Function to open an image file
+def open_image():
+    file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png *.jpg *.jpeg *.gif *.bmp")])
+    if file_path:
+        img = Image.open(file_path)
+        img.thumbnail((400, 400))  # Resize the image to fit in the window
+        img = ImageTk.PhotoImage(img)
+        image_label.config(image=img)
+        image_label.image = img
+
+
+# Create a button to open an image
+open_button = tk.Button(app, text="Open Image", command=open_image)
+open_button.pack(pady=10)
+
+
+# Create a label to display the image
+image_label = tk.Label(app)
+image_label.pack()
+
+
+# Start the Tkinter main loop
+app.mainloop()
