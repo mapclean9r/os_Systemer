1bc848cc62bdcaab444ff1299588580471227957
HomandoBruther <76850754+HomandoBruther@users.noreply.github.com>
2023-10-04 14:04:34 +0200

    utvided page test

--- src/gui/tkinter_image.py
+++ src/gui/tkinter_image.py
@@ -97,42 +97,6 @@ class main_page(tk.Frame):
         p1.show()
 
 
-'''
-# Application window
-app = tk.Tk()
-app.title("Bilde test")
-
-
-# Function to open file through filedialog
-def open_image():
-    file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png *.jpg *.jpeg *.gif *.bmp")])
-    if file_path:
-        img = Image.open(file_path)
-        img.thumbnail((400, 400))
-        img = ImageTk.PhotoImage(img)
-        image_label.config(image=img)
-        image_label.image = img
-
-
-# Button to open an image
-open_button = tk.Button(app, text="Open Image", command=open_image)
-open_button.pack(pady=10)
-
-
-# Button for navigation
-nav_button = tk.Button(app, text="To page test")
-
-
-# Label to display the image
-image_label = tk.Label(app)
-image_label.pack()
-
-
-# Tkinter main loop to run
-app.mainloop()
-'''
-
-
 if __name__ == "__main__":
     app = tk.Tk()
     main = main_page(app)
