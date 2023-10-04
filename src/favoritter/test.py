965d446ca9a1b8b0ae2927ae3acbe80641d0c7c6
HomandoBruther <76850754+HomandoBruther@users.noreply.github.com>
2023-10-04 11:16:25 +0200

    Update test.py

--- src/favoritter/test.py
+++ src/favoritter/test.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append('src')
 
-from userdata import bruker_def
-
+from src.userdata import bruker_def
+#import src.userdata.bruker_def as bruker_def
 
 bruker_def.test()
 bruker_def.lag_brukerkonto("DWADSDWDAWADA","DWDWADA","DWDWADAW",0)
