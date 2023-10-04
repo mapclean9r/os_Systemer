5b53147ed6908161b1b9e0c1f1d66515fc6bbf59
fredrikkri <fjkri@hotmail.com>
2023-10-04 11:53:15 +0200

    update
    eede

--- src/favoritter/test.py
+++ src/favoritter/test.py
@@ -1,7 +1,8 @@
-from src/userdata import bruker_def
+from userdata import bruker_def
 import sys
 sys.path.append('src')
 
-#TODO vi må få dette til å fungere
+
+# TODO vi må få dette til å fungere
 bruker_def.test()
 bruker_def.lag_brukerkonto("DWADSDWDAWADA", "DWDWADA", "DWDWADAW", 0)
