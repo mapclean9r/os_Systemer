353a6521c4dfea5c38275c04b407007ac7ff25df
HomandoBruther <76850754+HomandoBruther@users.noreply.github.com>
2023-10-04 13:22:01 +0200

    page navigation test tkinter

--- src/favoritter/test.py
+++ src/favoritter/test.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append('src')
 
-from src.userdata import bruker_def
-#import src.userdata.bruker_def as bruker_def
+
+from userdata import bruker_def
+
 
 bruker_def.test()
-bruker_def.lag_brukerkonto("DWADSDWDAWADA","DWDWADA","DWDWADAW",0)
-
+bruker_def.lag_brukerkonto("Jame", "Labron", "fake@fake.no", False)
