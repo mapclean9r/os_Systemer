ab1a0f073c7395aca286680545c4504805466b0f
fredrikkri <fjkri@hotmail.com>
2023-10-07 15:18:13 +0200

    alt er separert ut i egne filer og alt fungerer
    supere endringer

---
+++ src/login/login_details.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import messagebox
+from src.markedplaceApp import database_creation
+
+def login_checker(self):
+    username = self.entry_username.get()
+    password = self.entry_password.get()
+
+    database_creation.cursor.execute(
+        'SELECT * FROM users WHERE username=? AND password=?', (username, password))
+    user = database_creation.cursor.fetchone()
+
+    if user:
+        self.username = username
+        self.show_marketplace()
+    else:
+        messagebox.showerror("Error", "Incorrect username or password.")
+
+def display_login(self):
+    frame = tk.Frame(self)
+    frame.pack(pady=100)
+
+    lbl_title = tk.Label(frame, text="Login", font=("Arial", 24))
+    lbl_title.pack()
+
+    lbl_username = tk.Label(frame, text="Username")
+    lbl_username.pack(pady=5)
+    self.entry_username = tk.Entry(frame)
+    self.entry_username.pack(pady=5)
+
+    lbl_password = tk.Label(frame, text="Password")
+    lbl_password.pack(pady=5)
+    self.entry_password = tk.Entry(frame, show="*")
+    self.entry_password.pack(pady=5)
+
+    btn_login = tk.Button(frame, text="Login", command=self.login)
+    btn_login.pack(pady=20)
+    btn_register = tk.Button(frame, text="Register", command=self.register)
+    btn_register.pack()
--- src/markedplaceApp.py
+++ src/markedplaceApp.py
@@ -1,12 +1,7 @@
-import sqlite3
 import tkinter as tk
-from tkinter import messagebox, simpledialog, ttk
-from src.login import login_check
-from src.login.create_userprofile import making_a_user
-from src.login.user_logout import logout_a_user
+from src.login import login_details, user_logout, create_userprofile
+from src.markedplace_operations import tour_management
 from src.userdata import database_creation
-
-
 
 # GUI
 class MarketplaceApp(tk.Tk):
@@ -20,104 +15,25 @@ class MarketplaceApp(tk.Tk):
         self.show_login()
 
     def show_login(self):
-        frame = tk.Frame(self)
-        frame.pack(pady=100)
-
-        lbl_title = tk.Label(frame, text="Login", font=("Arial", 24))
-        lbl_title.pack()
-
-        lbl_username = tk.Label(frame, text="Username")
-        lbl_username.pack(pady=5)
-        self.entry_username = tk.Entry(frame)
-        self.entry_username.pack(pady=5)
-
-        lbl_password = tk.Label(frame, text="Password")
-        lbl_password.pack(pady=5)
-        self.entry_password = tk.Entry(frame, show="*")
-        self.entry_password.pack(pady=5)
-
-        btn_login = tk.Button(frame, text="Login", command=self.login)
-        btn_login.pack(pady=20)
-        btn_register = tk.Button(frame, text="Register", command=self.register)
-        btn_register.pack()
+        login_details.display_login(self)
 
     def login(self):
-        login_check.login_checker(self)
+        login_details.login_checker(self)
 
     def register(self):
-        making_a_user(self)
+        create_userprofile.making_a_user(self)
 
     def logout(self):
-        logout_a_user(self)
+        user_logout.logout_a_user(self)
 
     def show_marketplace(self):
-        for widget in self.winfo_children():
-            widget.destroy()
-
-        frame = tk.Frame(self)
-        frame.pack(pady=20)
-
-        lbl_title = tk.Label(
-            frame, text=f"Welcome, {self.username}", font=("Arial", 24))
-        lbl_title.pack(pady=20)
-
-        btn_offer_tour = tk.Button(
-            frame, text="Offer a Tour", command=self.offer_tour)
-        btn_offer_tour.pack(pady=20)
-
-        btn_logout = tk.Button(frame, text="Logout", command=self.logout)
-        btn_logout.pack(pady=20)
-
-        btn_delete_tour = tk.Button(
-            frame, text="Delete Tour", command=self.delete_tour)
-        btn_delete_tour.pack(pady=20)
-
-        self.tree = ttk.Treeview(frame, columns=(
-            'Title', 'Description', 'Offered by'))
-        self.tree.heading('Title', text='Title')
-        self.tree.heading('Description', text='Description')
-        self.tree.heading('Offered by', text='Offered by')
-        self.tree.pack(pady=20)
-
-        database_creation.cursor.execute(
-            'SELECT title, description, username FROM tours JOIN users ON tours.offered_by=users.id')
-        for tour in database_creation.cursor.fetchall():
-            self.tree.insert("", "end", values=tour)
+        tour_management.display_marketplace(self)
 
     def offer_tour(self):
-        title = simpledialog.askstring("Offer a Tour", "Enter tour title:")
-        description = simpledialog.askstring(
-            "Offer a Tour", "Enter tour description:")
-
-        database_creation.cursor.execute('SELECT id FROM users WHERE username=?',
-                       (self.username,))
-        user_id = database_creation.cursor.fetchone()[0]
-
-        database_creation.cursor.execute('INSERT INTO tours (title, description, offered_by) VALUES (?, ?, ?)',
-                       (title, description, user_id))
-        database_creation.conn.commit()
-
-        self.tree.insert("", "end", values=(title, description, self.username))
-        messagebox.showinfo("Success", "Tour offered successfully.")
+        tour_management.offering_a_tour(self)
 
     def delete_tour(self):
-        selected_tour = self.tree.selection()
-
-        if not selected_tour:
-            messagebox.showerror("Error", "No tour selected.")
-            return
-
-        confirm = messagebox.askyesno(
-            "Delete Tour", "Are you sure you want to delete the selected tour?")
-        if not confirm:
-            return
-
-        tour_title = self.tree.item(selected_tour)['values'][0]
-        database_creation.cursor.execute("DELETE FROM tours WHERE title=?", (tour_title,))
-        database_creation.conn.commit()
-
-        self.tree.delete(selected_tour)
-        messagebox.showinfo("Success", "Tour deleted successfully.")
+        tour_management.deleting_a_tour(self)
 
 if __name__ == '__main__':
     database_creation.create_database()
